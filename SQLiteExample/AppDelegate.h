47d94163402e66b621237fff7b941bcf62645a4c
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2015-02-19 00:33:52 -0800

    Initial Commit

---
+++ SQLiteExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLiteExample
+//
+//  Created by Erick Bennett on 2/19/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLiteExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLiteExample
+//
+//  Created by Erick Bennett on 2/19/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
